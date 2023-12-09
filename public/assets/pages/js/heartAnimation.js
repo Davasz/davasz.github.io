79b46ed8b48f1a0148f61f7432ea0d2fcaa939eb
Davasz <davidavi200615@gmail.com>
2023-12-09 17:14:30 -0300

    ⚙️feat: Sistema de contagem do coração

--- public/assets/pages/js/heartAnimation.js
+++ public/assets/pages/js/heartAnimation.js
@@ -4,10 +4,25 @@ const miniHeartContainer1 = document.querySelector('.mini-heart-animation-contai
 const miniHeartContainer2 = document.querySelector('.mini-heart-animation-container-2');
 const miniHeartContainer3 = document.querySelector('.mini-heart-animation-container-3');
 
+const count = document.querySelector('.count')
+
+const attCount = () => {
+  fetch('/count')
+    .then(response => response.json())
+    .then(data => {
+      count.textContent = "#" + data.count
+    })
+}
+
 document.querySelector('.heart').addEventListener('click', () => {
+  console.log("clicou")
   bigHeart.classList.toggle('heart-click');
   miniHeartArr.map(el => el.classList.toggle('mini-heart-click'));
   miniHeartContainer1.classList.toggle('mini-heart-container-click-1');
   miniHeartContainer2.classList.toggle('mini-heart-container-click-2');
   miniHeartContainer3.classList.toggle('mini-heart-container-click-3');
+
+  attCount()
 })
+
+attCount()
