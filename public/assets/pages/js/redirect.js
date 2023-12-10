bf5526c5cc3afee1f392495e8dfda9c92e66ed19
Davasz <davidavi200615@gmail.com>
2023-12-10 15:24:00 -0300

    ⚙️feat: Sistema de redirecionamento

---
+++ public/assets/pages/js/redirect.js
@@ -0,0 +1,19 @@
+function redirect(local) {
+    switch (local) {
+        case 1:
+            window.location.href = "https://www.instagram.com/p/Czw4OVeJyqV/?img_index=1";
+            break
+        case 2:
+            window.location.href = "https://github.com/Davasz/JWT-AUTENTICACAO-NODE"
+            break
+        case 3:
+            window.location.href = "https://github.com/Davasz/CampoMinado"
+            break
+        case 4:
+            window.location.href = "https://davisouza.netlify.app"
+            break
+        case 5:
+            window.location.href = "https://pokedex-3nft.onrender.com"
+            break
+    }
+}
