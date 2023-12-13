46d79bc66ec519537a1bf4d668c431a9e8d620f2
Davasz <davidavi200615@gmail.com>
2023-12-12 22:10:01 -0300

    ♻️refactor: Utilização do firebase como core do projeto

--- public/assets/pages/js/heartAnimation.js
+++ public/assets/pages/js/heartAnimation.js
@@ -1,39 +1,51 @@
+import { database, ref, get, runTransaction } from "../../../../config/firebaseConfig.js"
+
 const bigHeart = document.querySelector('.heart')
 const miniHeartArr = [...document.querySelectorAll('.mini-heart')]
 const miniHeartContainer1 = document.querySelector('.mini-heart-animation-container-1')
 const miniHeartContainer2 = document.querySelector('.mini-heart-animation-container-2')
 const miniHeartContainer3 = document.querySelector('.mini-heart-animation-container-3')
-
 const count = document.querySelector('.count')
-let clicks = 0
+
+const countRef = ref(database, 'count');
 
 const getCount = () => {
-  fetch('/count')
-    .then(response => response.json())
-    .then(data => {
-      count.textContent = "#" + data.count
-    })
+  get(countRef).then((snapshot) => {
+    const countValue = snapshot.val();
+    count.textContent = "#" + countValue
+  })
 }
 
-// Função para atualizar o contador de curtidas
-const attCount = () => {
-  fetch('/count/att')
-  clicks += 1
+const updateCount = (value) => {
+  runTransaction(countRef, (currentCount) => {
+    if (currentCount <= 0) {
+      return currentCount = 0
+    }
+    if (value) {
+      return currentCount - 1
+    }
+    return currentCount + 1
+  }).then(() => {
+    getCount()
+  })
+}
+
+const toggleHeart = () => {
+  bigHeart.classList.toggle('heart-click')
+  miniHeartArr.map(el => el.classList.toggle('mini-heart-click'))
+  miniHeartContainer1.classList.toggle('mini-heart-container-click-1')
+  miniHeartContainer2.classList.toggle('mini-heart-container-click-2')
+  miniHeartContainer3.classList.toggle('mini-heart-container-click-3')
 }
 
 document.querySelector('.heart').addEventListener('click', () => {
-  if (!(clicks >= 5)) {
-    bigHeart.classList.toggle('heart-click')
-    miniHeartArr.map(el => el.classList.toggle('mini-heart-click'))
-    miniHeartContainer1.classList.toggle('mini-heart-container-click-1')
-    miniHeartContainer2.classList.toggle('mini-heart-container-click-2')
-    miniHeartContainer3.classList.toggle('mini-heart-container-click-3')
-    console.log(bigHeart.classList.contains('heart-click'))
+  toggleHeart()
 
-    if (bigHeart.classList.contains('heart-click')) {
-      attCount()
-    }
-    getCount()
+  if (bigHeart.classList.contains('heart-click')) {
+    updateCount(false)
+  }
+  if (!(bigHeart.classList.contains('heart-click'))) {
+    updateCount(true)
   }
 })
 
